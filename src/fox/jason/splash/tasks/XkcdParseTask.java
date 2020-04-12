d68e837725f7854949c0b608860d4461d0de9caf
Jason Fox <jason.fox@fiware.org>
2020-04-12 19:34:44 +0200

    Remove Cats-Parse, XKCD-Parse and XKCD-Select scripts.
    Replace script with Ant Tasks.

---
+++ src/fox/jason/splash/tasks/XkcdParseTask.java
@@ -0,0 +1,44 @@
+/*
+ *  This file is part of the DITA-OT Passthrough Plug-in project.
+ *  See the accompanying LICENSE file for applicable licenses.
+ */
+
+package fox.jason.splash.tasks;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+//   This function extracts the image title and URL from the XKCD API
+//   response. The parsing has been done using simple string manipulation instead
+//   of loading the data as JSON.
+
+public class XkcdParseTask extends Task {
+
+
+	/**
+	 * Creates a new <code>XkcdParseTask</code> instance.
+	 */
+	public XkcdParseTask() {
+		super();
+	}
+
+	
+	/**
+     * Method execute.
+     *
+     * @throws BuildException if something goes wrong
+     */
+	@Override
+    public void execute(){
+
+		String input = project.getProperty("xkcd.info");
+		int posImgUrl = input.indexOf("https://imgs.xkcd.com");
+		int lenImgUrl = input.indexOf('"', posImgUrl);
+		int posTitle = input.indexOf("\"title\": ");
+		int lenTitle = input.indexOf('"', posTitle + 10);
+
+		getProject().setProperty("splash.image.url", input.substring(posImgUrl, lenImgUrl));
+		getProject().setProperty("splash.title", input.substring(posTitle + 10, lenTitle));
+
+	}
+}
---
+++ src/fox/jason/splash/tasks/XkcdSelectTask.java
@@ -0,0 +1,45 @@
+/*
+ *  This file is part of the DITA-OT Passthrough Plug-in project.
+ *  See the accompanying LICENSE file for applicable licenses.
+ */
+
+package fox.jason.splash.tasks;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+//   This function extracts the num count from the XKCD API
+//   response and then selects a random entry. The parsing has
+//.  been done using simple string manipulation instead
+//   of loading the data as JSON.
+
+public class XkcdSelectTask extends Task {
+
+	/**
+	 * Creates a new <code>XkcdSelectTask</code> instance.
+	 */
+	public XkcdSelectTask() {
+		super();
+	}
+
+	/**
+     * Method execute.
+     *
+     * @throws BuildException if something goes wrong
+     */
+	@Override
+    public void execute(){
+
+		String input = getProject().getProperty("xkcd.latest");
+		int pos = input.indexOf("\"num\": ");
+		int len = input.indexOf(",", pos);
+
+		getProject().setProperty(
+		  "num",
+		  Integer.toString((int) Math.floor(
+		  		Math.random() * Integer.parseInt(input.substring(pos + 7, len)) + 1)
+		  	)
+		);
+
+	}
+}
